6bb2d6acb601946863dfb7a442db1cfb076b5de5
emanh1 <23426946+emanh1@users.noreply.github.com>
2026-05-23 16:21:23 +0700

    dont always fetch user on all events

--- app/plugins/session.client.ts
+++ app/plugins/session.client.ts
@@ -3,11 +3,14 @@ export default defineNuxtPlugin(async () => {
   const supabase = useSupabaseClient();
   await userStore.fetch();
 
-  supabase.auth.onAuthStateChange(async (_, session) => {
-    if (session?.user) {
-      await userStore.fetch();
-    } else {
+  supabase.auth.onAuthStateChange(async (event, session) => {
+    if (event === "SIGNED_OUT") {
       userStore.clear();
+      return;
+    }
+    
+    if (session?.user && (event === "SIGNED_IN" || event === "USER_UPDATED")) {
+      await userStore.fetch();
     }
   });
 });
